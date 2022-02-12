d0eb05501b9c7287b29bcd5d5f20704e0b014ee7
AmohPrince <Amohprincethedoctor@gmail.com>
2022-02-12 18:40:28 +0300

    Completed the cart  layout  but its full of errors

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Container, Typography, Button, Grid } from "@material-ui/core";
+import useStyles from "./styles";
+import Cartitem from "./Cartitem/Cartitem";
+
+const Cart = ({ cart }) => {
+  const classes = useStyles();
+
+  const EmptyCart = () => {
+    <Typography variant="subtitle1">
+      You have no items in your shopping cart , start adding some
+    </Typography>;
+  };
+
+  const FilledCart = () => (
+    <>
+      <Grid container spacing={3}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <Cartitem item={item} />
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h4">
+          SubTotal = {cart.subtotal.formatted_with_symbol}
+        </Typography>
+        <div>
+          <Button
+            className={classes.emptyButton}
+            size="large"
+            type="button"
+            variant="contained"
+            color="secondary"
+          >
+            Empty cart
+          </Button>
+          <Button
+            className={classes.checkoutButton}
+            size="large"
+            type="button"
+            variant="Primary"
+            color="secondary"
+          >
+            Checkout
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+
+  if (!cart.line_items) return "Loading...";
+  return (
+    <Container>
+      <div className={classes.toolbar} />
+      <Typography className={classes.title} variant="h3" gutterBottom>
+        Your Shopping Cart
+      </Typography>
+      {cart.line_items.length === 0 ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
+
+export default Cart;
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,35 +4,35 @@ import Product from "./Product/Product";
 import { Grid } from "@material-ui/core";
 import useStyles from "./styles";
 
-const products_array = [
-  {
-    id: 1,
-    name: "shoes",
-    description: "Running shoes",
-    price: "$5",
-    image:
-      "https://images.pexels.com/photos/2759783/pexels-photo-2759783.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-  },
-  {
-    id: 2,
-    name: "macbook",
-    description: "Apple Macbook",
-    price: "$10",
-    image:
-      "https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-  },
-];
+// const products_array = [
+//   {
+//     id: 1,
+//     name: "shoes",
+//     description: "Running shoes",
+//     price: "$5",
+//     image:
+//       "https://images.pexels.com/photos/2759783/pexels-photo-2759783.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+//   },
+//   {
+//     id: 2,
+//     name: "macbook",
+//     description: "Apple Macbook",
+//     price: "$10",
+//     image:
+//       "https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+//   },
+// ];
 
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
   const classes = useStyles();
   return (
     <main className={classes.content}>
       <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
-        {products_array.map((product) => {
+        {products.map((product) => {
           return (
             <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-              <Product product={product} />
+              <Product product={product} onAddToCart={onAddToCart} />
             </Grid>
           );
         })}
