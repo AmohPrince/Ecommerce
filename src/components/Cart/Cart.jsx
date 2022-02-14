7e150c94edca34a1c988ba105686d381886b6621
AmohPrince <Amohprincethedoctor@gmail.com>
2022-02-14 16:58:11 +0300

    changed a few errors

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Typography, Button, Grid } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import CartItem from "./CartItem/CartItem";
+import CartItem from "./Cartitem/CartItem";
 import useStyles from "./styles";
 
 const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
